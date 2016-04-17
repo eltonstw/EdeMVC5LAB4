e05c6edb5a1f8665f98d6cfc85df640e62374613
eltonstw <eltons@edetw.com>
2016-04-18 00:34:37 +0800

    Initial

---
+++ LAB/LAB.WEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LAB.WEB.MvcApplication" Language="C#" %>
---
+++ LAB/LAB.WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LAB.WEB.Startup))]
+namespace LAB.WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
